eab5a5af396fcf5ae4d5938d3b285ccf9069a641
ftmhabt <ftmh.a78@gmail.com>
2024-03-03 20:59:40 +0330

    Add sidebar component

--- cv-builder/src/main.jsx
+++ cv-builder/src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './components/header'
 import './index.css'
+import Forms from './components/forms'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header/>
-
+    <Forms/>
   </React.StrictMode>,
 )
