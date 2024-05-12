e3321b483e52e17d900734043fe9297fbb047c94
ftmhabt <ftmh.a78@gmail.com>
2024-05-12 23:19:05 +0330

    Fix design bug

--- cv-builder/src/components/delete-button.jsx
+++ cv-builder/src/components/delete-button.jsx
@@ -41,7 +41,7 @@ export default function DeleteButton({ arrayName, id, type = "form" }) {
       }}
     >
       delete
-      {type === "form" ? "form" : ""}
+      {type === "form" ? " form" : ""}
     </button>
   );
 }
