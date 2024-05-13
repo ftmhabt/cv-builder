fe22c50987a6938bed1f7b071b4bdf2bb03a73e9
ftmhabt <ftmh.a78@gmail.com>
2024-05-13 22:52:55 +0330

    Add export pdf strcture

--- cv-builder/src/components/pdf.jsx
+++ cv-builder/src/components/pdf.jsx
@@ -1,4 +1,14 @@
 import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
+import { useContext } from "react";
+import cvContext from "../../context/general-context";
+import Icon from "@mdi/react";
+import {
+  mdiCalendarRange,
+  mdiMapMarkerOutline,
+  mdiEyeOutline,
+  mdiXml,
+  mdiCalendarRange,
+} from "@mdi/js";
 
 const styles = StyleSheet.create({
   theme: {
@@ -103,15 +113,166 @@ const styles = StyleSheet.create({
   //   }
 });
 
-export const MyDocument = () => (
-  <Document>
-    <Page size="A4" style={styles.page}>
-      <View style={styles.section}>
-        <Text>Section #1</Text>
-      </View>
-      <View style={styles.section}>
-        <Text>Section #2</Text>
-      </View>
-    </Page>
-  </Document>
-);
+export const MyDocument = () => {
+  const context = useContext(cvContext);
+  let [first, last] = context.name.split(" ");
+
+  return (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <div style={styles.personal}>
+          <div style={styles.name}>
+            <div style={styles.first}>{first}</div>
+            <div style={styles.last}>{last}</div>
+          </div>
+          <div style={styles.info}>
+            {context.email && <div>Email:</div>}
+            <div style={styles.theme}>{context.email}</div>
+            {context.linkedin && <div>LinkedIn:</div>}
+            <div style={styles.theme}>{context.linkedin}</div>
+          </div>
+        </div>
+        {context.about && (
+          <div style={styles.head}>
+            Profile <hr />
+          </div>
+        )}
+        <div style={[styles.about, styles.odd]}>{context.about}</div>
+        {context.exprience.length > 0 && (
+          <div style={styles.head}>
+            Work Exprience
+            <hr />
+          </div>
+        )}
+        {context.exprience.length > 0 && (
+          <div style={styles.odd}>
+            {context.exprience.map((item) => (
+              <div key={item.id} style={styles.flexcol}>
+                <div style={styles.bold}>{item.role}</div>
+                <div style={styles.bold}>{item.company}</div>
+                <div style={[styles.theme, styles.flex, styles.thin]}>
+                  {item.from && item.to && (
+                    <div style={styles.flex}>
+                      <Icon path={mdiCalendarRange} size={0.4} />
+                      <div>{item.from}</div>
+                      <div>-</div>
+                      <div>{item.to}</div>
+                    </div>
+                  )}
+                  {item.location && (
+                    <div style={styles.flex}>
+                      <Icon path={mdiMapMarkerOutline} size={0.4} />
+                      <div>{item.location}</div>
+                    </div>
+                  )}
+                </div>
+                <div>{item.details}</div>
+                {item.stack.length > 0 && (
+                  <div style={[styles.flex, styles.stack]}>
+                    Stack:
+                    {item.stack.map((stack, index) => (
+                      <div key={index}>
+                        {stack}
+                        {index === item.stack.length - 1 ? "" : " / "}
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        )}
+        {context.projects.length > 0 && (
+          <div style={styles.head}>
+            Projects
+            <hr />
+          </div>
+        )}
+        {context.projects.length > 0 && (
+          <div style={styles.odd}>
+            {context.projects.map((item) => (
+              <div key={item.id} style={styles.flexcol}>
+                <div style={styles.bold}>{item.name}</div>
+                <div style={[styles.flex, styles.theme]}>
+                  {item.demo && (
+                    <div style={styles.flex}>
+                      <Icon path={mdiEyeOutline} size={0.4} />
+                      <a href={item.demo} target="_blank">
+                        demo
+                      </a>
+                    </div>
+                  )}
+                  {item.github && (
+                    <div style={styles.flex}>
+                      <Icon path={mdiXml} size={0.4} />
+                      <a href={item.github} target="_blank">
+                        github
+                      </a>
+                    </div>
+                  )}
+                </div>
+                <div>{item.details}</div>
+                {item.stack.length > 0 && (
+                  <div style={[styles.flex, styles.theme]}>
+                    Stack:
+                    {item.stack.map((stack, index) => (
+                      <div key={index}>
+                        {stack}
+                        {index === item.stack.length - 1 ? "" : " / "}
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        )}
+        {context.education.length > 0 && (
+          <div style={styles.head}>
+            Education <hr />
+          </div>
+        )}
+        {context.education.length > 0 && (
+          <div style={[styles.education, styles.odd]}>
+            {context.education.map((item) => (
+              <div key={item.id} style={styles.flexcol}>
+                <div style={styles.bold}>{item.field}</div>
+                <div style={styles.bold}>{item.univercity}</div>
+                <div style={[styles.flex, styles.theme, styles.thin]}>
+                  {item.from && item.to && (
+                    <div style={styles.flex}>
+                      <Icon path={mdiCalendarRange} size={0.4} />
+                      <div>{item.from}</div>
+                      <div>-</div>
+                      <div>{item.to}</div>
+                    </div>
+                  )}
+                  {item.location && (
+                    <div style={styles.flex}>
+                      <Icon path={mdiMapMarkerOutline} size={0.4} />
+                      <div>{item.location}</div>
+                    </div>
+                  )}
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+        {context.skills.length > 0 && (
+        <div style={styles.head}>
+          Skills
+          <hr />
+        </div>
+      )}
+      {context.skills.length > 0 && (
+        <div style={[styles.skillpreview,styles.odd]}>
+          {context.skills.map((item) => (
+            item.name&&
+            <div key={item.id}>{item.name}</div>
+          ))}
+        </div>
+      )}
+      </Page>
+    </Document>
+  );
+};
