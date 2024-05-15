af727350a6c05da5581418fa074ba2f101ec856d
ftmhabt <ftmh.a78@gmail.com>
2024-05-15 15:34:53 +0330

    Fix pdf style bugs

--- cv-builder/src/components/pdf.jsx
+++ cv-builder/src/components/pdf.jsx
@@ -1,4 +1,12 @@
-import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
+import {
+  Page,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+  Svg,
+  Path,
+} from "@react-pdf/renderer";
 import Icon from "@mdi/react";
 import {
   mdiCalendarRange,
@@ -71,7 +79,7 @@ const styles = StyleSheet.create({
 
   flex: {
     flexDirection: "row",
-    gap: 10,
+    gap: 5,
     alignItems: "center",
   },
 
@@ -126,6 +134,12 @@ const styles = StyleSheet.create({
   },
 });
 
+const Calender = () => (
+  <Svg width="10" height="10" viewBox="0 0 24 24">
+    <Path d="M9,10H7V12H9V10M13,10H11V12H13V10M17,10H15V12H17V10M19,3H18V1H16V3H8V1H6V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3M19,19H5V8H19V19Z" />
+  </Svg>
+);
+
 export const MyDocument = ({ context }) => {
   let [first, last] = context.name.split(" ");
 
@@ -144,155 +158,158 @@ export const MyDocument = ({ context }) => {
             <Text style={styles.theme}>{context.linkedin}</Text>
           </View>
         </View>
-
         {context.about && (
           <View style={styles.flex}>
             <Text style={styles.head}>Profile</Text>
             <View style={[styles.line, styles.line1]}></View>
           </View>
         )}
-
         <View style={styles.wrap}>
           <Text style={[styles.about, styles.odd]}>{context.about}</Text>
         </View>
-        {context.exprience.length > 0 ? (
+
+        {context.exprience.length > 0 && (
           <View style={styles.flex}>
             <Text style={styles.head}>Work Exprience </Text>
             <View style={[styles.line, styles.line2]}></View>
           </View>
-        ) : null}
+        )}
         {context.exprience.length > 0 && (
           <View style={styles.odd}>
             {context.exprience.map((item) => (
               <View key={item.id} style={styles.flexcol}>
                 <Text style={styles.bold}>{item.role}</Text>
                 <Text style={styles.bold}>{item.company}</Text>
-                <Text style={[styles.theme, styles.flex, styles.thin]}>
-                  {item.from && item.to ? (
-                    <Text style={styles.flex}>
-                      <Icon path={mdiCalendarRange} size={0.4} />
+                <View style={[styles.theme, styles.flex, styles.thin]}>
+                  {item.from && item.to && (
+                    <View style={styles.flex}>
+                      <Calender />
                       <Text>{item.from}</Text>
                       <Text>-</Text>
                       <Text>{item.to}</Text>
-                    </Text>
-                  ) : null}
-                  {item.location ? (
-                    <Text style={styles.flex}>
+                    </View>
+                  )}
+                  {item.location && (
+                    <View style={styles.flex}>
                       <Icon path={mdiMapMarkerOutline} size={0.4} />
                       <Text>{item.location}</Text>
-                    </Text>
-                  ) : null}
-                </Text>
+                    </View>
+                  )}
+                </View>
                 <Text style={styles.wrap}>{item.details}</Text>
-                {item.stack.length > 0 ? (
-                  <Text style={[styles.flex, styles.stack]}>
-                    Stack:
+                {item.stack.length > 0 && (
+                  <View style={[styles.flex, styles.stack]}>
+                    <Text>Stack:</Text>
                     {item.stack.map((stack, index) => (
                       <Text key={index}>
                         {stack}
-                        {index === item.stack.length - 1 ? "" : " / "}
+                        {index === item.stack.length - 1 ? "" : "  /"}
                       </Text>
                     ))}
-                  </Text>
-                ) : null}
+                  </View>
+                )}
               </View>
             ))}
           </View>
         )}
+
         {context.projects.length > 0 && (
           <View style={styles.flex}>
             <Text style={styles.head}>Projects </Text>
             <View style={[styles.line, styles.line3]}></View>
           </View>
         )}
-        {context.projects.length > 0 ? (
+        {context.projects.length > 0 && (
           <View style={styles.odd}>
             {context.projects.map((item) => (
               <View key={item.id} style={styles.flexcol}>
                 <Text style={styles.bold}>{item.name}</Text>
-                <Text style={[styles.flex, styles.theme]}>
-                  {item.demo ? (
-                    <Text style={styles.flex}>
+                <View style={[styles.flex, styles.theme]}>
+                  {item.demo && (
+                    <View style={styles.flex}>
                       <Icon path={mdiEyeOutline} size={0.4} />
                       <Text href={item.demo} target="_blank">
                         demo
                       </Text>
-                    </Text>
-                  ) : null}
+                    </View>
+                  )}
                   {item.github && (
-                    <Text style={styles.flex}>
+                    <View style={styles.flex}>
                       <Icon path={mdiXml} size={0.4} />
                       <Text href={item.github} target="_blank">
                         github
                       </Text>
-                    </Text>
+                    </View>
                   )}
-                </Text>
+                </View>
                 <Text style={styles.wrap}>{item.details}</Text>
-                {item.stack.length > 0 ? (
-                  <Text style={[styles.flex, styles.stack]}>
-                    Stack:
+                {item.stack.length > 0 && (
+                  <View style={[styles.flex, styles.stack]}>
+                    <Text>Stack:</Text>
                     {item.stack.map((stack, index) => (
                       <Text key={index}>
                         {stack}
-                        {index === item.stack.length - 1 ? "" : " / "}
+                        {index === item.stack.length - 1 ? "" : "  /"}
                       </Text>
                     ))}
-                  </Text>
-                ) : null}
+                  </View>
+                )}
               </View>
             ))}
           </View>
-        ) : null}
-        {context.education.length > 0 ? (
+        )}
+
+        {context.education.length > 0 && (
           <View style={styles.flex}>
             <Text style={styles.head}>Education </Text>
             <View style={[styles.line, styles.line4]}></View>
           </View>
-        ) : null}
-        {context.education.length > 0 ? (
+        )}
+        {context.education.length > 0 && (
           <View style={[styles.education, styles.odd]}>
             {context.education.map((item) => (
               <View key={item.id} style={styles.flexcol}>
                 <Text style={styles.bold}>{item.field}</Text>
                 <Text style={styles.bold}>{item.univercity}</Text>
-                <Text style={[styles.flex, styles.theme, styles.thin]}>
-                  {item.from && item.to ? (
-                    <Text style={styles.flex}>
-                      <Icon path={mdiCalendarRange} size={0.4} />
+                <View style={[styles.flex, styles.theme, styles.thin]}>
+                  {item.from && item.to && (
+                    <View style={styles.flex}>
+                      <Calender />
                       <Text>{item.from}</Text>
                       <Text>-</Text>
                       <Text>{item.to}</Text>
-                    </Text>
-                  ) : null}
-                  {item.location ? (
-                    <Text style={styles.flex}>
+                    </View>
+                  )}
+                  {item.location && (
+                    <View style={styles.flex}>
                       <Icon path={mdiMapMarkerOutline} size={0.4} />
                       <Text>{item.location}</Text>
-                    </Text>
-                  ) : null}
-                </Text>
+                    </View>
+                  )}
+                </View>
               </View>
             ))}
           </View>
-        ) : null}
-        {context.skills.length > 0 ? (
+        )}
+
+        {context.skills.length > 0 && (
           <View style={styles.flex}>
             <Text style={styles.head}>Skills </Text>
             <View style={[styles.line, styles.line5]}></View>
           </View>
-        ) : null}
-        {context.skills.length > 0 ? (
+        )}
+        {context.skills.length > 0 && (
           <View style={styles.skillpreview}>
-            {context.skills.map((item) =>
-              item.name ? (
-                <Text key={item.id} style={styles.skill}>
-                  {item.name}
-                </Text>
-              ) : null
+            {context.skills.map(
+              (item) =>
+                item.name && (
+                  <Text key={item.id} style={styles.skill}>
+                    {item.name}
+                  </Text>
+                )
             )}
           </View>
-        ) : null}
+        )}
       </Page>
     </Document>
   );
